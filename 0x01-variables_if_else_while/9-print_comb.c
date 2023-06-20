358853e313f07def654b698adbd3c900d6c2479e
djphycastrous <paulgodwin798@gmail.com>
2023-06-19 21:11:43 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'A program that print number of base 10 using putchar'
+ * Description: 'A program that print possible combinations of single-digit'
  * Return: always (0)
  */
 int main(void)
+{
+	int numb = 0;
+
+	while (numb <= 9)
+	{
+		putchar(numb + 48);
+
+		if (numb != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++numb;
+	}
+	putchar('\n');
+	return (0);
+}
